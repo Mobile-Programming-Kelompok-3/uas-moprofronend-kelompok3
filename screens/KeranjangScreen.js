10a6e04fb0057e315b4ef48a597954f1a1ca37f6
Asysyakur <hilmiasysyakur123@upi.edu>
2023-11-14 13:55:29 +0700

    pertama

---
+++ screens/KeranjangScreen.js
@@ -0,0 +1,14 @@
+import { ScrollView, Text, View } from 'react-native'
+import React from 'react'
+import KeranjangCard from '../components/KeranjangCard'
+
+
+function KeranjangScreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Keranjang Dlillah!!</Text>
+      </View>
+    );
+  }
+
+export default KeranjangScreen
