2c907d35def957723b71dae7809833e51e16b3f3
idayantti <idayanti@ypi.edu>
2023-11-16 14:43:39 +0700

    update RiwayatPesan

---
+++ screens/RiwayatPesan.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button, View, Text } from 'react-native';
+
+function RiwayatPesan() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Keranjang Dlillah!!</Text>
+      </View>
+    );
+  }
+
+export default RiwayatPesan;
