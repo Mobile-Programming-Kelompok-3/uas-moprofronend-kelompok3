d54bfea4c7c15c9775623ba7d666d872eaa36a4e
idayantti <idayanti@upi.edu>
2023-11-18 19:56:08 +0700

    update ida

--- screens/RiwayatPesan.js
+++ screens/RiwayatPesan.js
@@ -3,10 +3,11 @@ import { Button, View, Text } from 'react-native';
 
 function RiwayatPesan() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Riwayat Belanja</Text>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' , fontSize: '17'}}>
+        <Text style={{fontFamily: 'Poppins', marginLeft: 25, marginTop: 10, fontWeight: 'semi-bold'}}>Riwayat Transaksi</Text>
+
       </View>
     );
-  }
+}
 
 export default RiwayatPesan;
