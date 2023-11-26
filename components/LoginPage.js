6024cc8813d6a9b891b3bc40b4ddb92092de4aed
kiranasyafa12 <kirana28.ks@gmail.com>
2023-11-26 15:29:05 +0700

    lp

---
+++ components/LoginPage.js
@@ -0,0 +1,13 @@
+import {Text, View, Image} from 'react-native';
+import React from 'react';
+import {colors} from '../Constant';
+  
+const styles = StyleSheet.create({
+    header: {
+    width: '100%',
+    height: 60,
+    backgroundColor: '#80AAFC',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+});
