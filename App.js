2ad1ce86885012fc521a8ed895cdc7e6f55c8670
Istioktav21 <istioktaviani7@upi.edu>
2023-11-26 10:17:45 +0700

    app

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import Homescreen from "./screens/Homescreen";
 import ProfilScreen from "./screens/ProfilScreen";
 import KeranjangScreen from "./screens/KeranjangScreen";
 import RiwayatPesan from "./screens/RiwayatPesan";
+import ProductDetail from "./components/ProductDetail";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -15,16 +16,25 @@ function RiwayatStack() {
     <Stack.Navigator >
       <Stack.Screen name="Profil" component={ProfilScreen} />
       <Stack.Screen name="Riwayat Transaksi" component={RiwayatPesan} />
+      
     </Stack.Navigator>
   );
 }
 
+function MenuStack() {
+  return (
+    <Stack.Navigator >
+      <Stack.Screen name="Menu" component={Homescreen} />
+      <Stack.Screen name="Product Detail" component={ProductDetail} />
+      </Stack.Navigator >
+  );
+}
 function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator screenOptions={{ headerShown: false }}>
         <Tab.Screen name="Keranjang" component={KeranjangScreen} />
-        <Tab.Screen name="Home" component={Homescreen} />
+        <Tab.Screen name="Home" component={MenuStack} />
         <Tab.Screen name="Profil" component={RiwayatStack} />
       </Tab.Navigator>
     </NavigationContainer>
