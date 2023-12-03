045338528e18d606678d9deb38e5a0f77fb285fb
kiranasyafa12 <kirana28.ks@gmail.com>
2023-12-03 20:06:11 +0700

    login

--- screens/Login.js
+++ screens/Login.js
@@ -1,63 +1,18 @@
-// import React, { useState } from 'react';
-// import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
-// import Login from './screens/Login';
-// import RegisterPage from '../components/RegisterPage';
-
-// const LoginPage = ({ navigation }) => {
-//   const [username, setUsername] = useState('');
-//   const [password, setPassword] = useState('');
-
-//   const handleLogin = () => {
-//     console.log(`Username: ${username}, Password: ${password}`);
-//   };
-
-//   const handleRegisterLink = () => {
-//     navigation.navigate('RegisterPage');
-//   };
-
-//   return (
-//     <View style={styles.container}>
-//       <Text style={{ justifyContent: 'center', alignItems: 'center' }}>Login Akun</Text>
-//       <TextInput
-//         style={styles.input}
-//         placeholder="Username"
-//         onChangeText={(text) => setUsername(text)}
-//         value={username}
-//       />
-//       <TextInput
-//         style={styles.input}
-//         placeholder="Password"
-//         secureTextEntry
-//         onChangeText={(text) => setPassword(text)}
-//         value={password}
-//       />
-//       <TouchableOpacity style={styles.button} onPress={handleLogin}>
-//         <Text style={styles.buttonText}>Login</Text>
-//       </TouchableOpacity>
-//       <TouchableOpacity onPress={handleRegisterLink}>
-//         <Text>Don't have an account? Register here.</Text>
-//       </TouchableOpacity>
-//     </View>
-//   );
-// };
-
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { Image, View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
   const handleLogin = () => {
-    // Logika autentikasi dapat ditambahkan di sini
     console.log('Username:', username);
     console.log('Password:', password);
-    // Tambahkan logika autentikasi sesuai kebutuhan aplikasi Anda
   };
 
   return (
     <View style={styles.container}>
-      <Text style={{fontFamily: 'Poppins', marginLeft: 25, marginTop: 10, fontWeight: 'bold'}}>Selamat datang!</Text>
+      <Text style={{fontFamily: 'Poppins', marginLeft: 25, marginTop: 10, fontWeight: 'bold'}}>BeKi</Text>
       <TextInput
         style={styles.input}
         placeholder="Username"
